1ba06fd5ee7359d965b8e809d8eb4253c3812fec
David Isaksson <davidisaksson93@gmail.com>
2023-07-02 12:34:23 +0200

    Add code to derive key with Scrypt and to generate TOTPs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use base64::{engine::general_purpose, Engine as _};
+use hex::FromHex;
+use libreauth::oath::TOTPBuilder;
+use scrypt::{
+    password_hash::{PasswordHasher, SaltString},
+    Scrypt,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let password = b"hunter42";
+    let salt_string = "ABCABCABCABC";
+
+    let salt = SaltString::from_b64(
+        &general_purpose::STANDARD_NO_PAD
+            .encode(Vec::from_hex(salt_string).expect("Failed to decode hex")),
+    )
+    .expect("Failed to parse salt");
+
+    let scrypt_params = scrypt::Params::new(15, 8, 1, 32).expect("Failed to set scrypt params");
+    let derived_key = Scrypt
+        .hash_password_customized(password, None, None, scrypt_params, &salt)
+        .unwrap();
+    println!("{:?}", derived_key.hash.unwrap());
+
+    let key = "ABCABCABCABC".to_string();
+    let code = TOTPBuilder::new()
+        .base32_key(&key)
+        .finalize()
+        .unwrap()
+        .generate();
+    assert_eq!(code.len(), 6);
+    println!("{}", code);
 }
