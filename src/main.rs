1fab70df2d09dd902f35175c2a90065c4ccb02e6
David Isaksson <davidisaksson93@gmail.com>
2023-07-23 10:58:03 +0200

    Parse into structs instead of maps

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ use scrypt::{
     password_hash::{PasswordHasher, SaltString},
     Scrypt,
 };
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
 use std::{
@@ -21,7 +20,7 @@ use std::{
     time::{SystemTime, UNIX_EPOCH},
 };
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize)]
 struct EntryInfo {
     secret: String,
     algo: String,
@@ -29,27 +28,59 @@ struct EntryInfo {
     period: i32,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize)]
 struct Entry {
-    // #[serde(rename = "type")]
-    // type_: String,
-    // uuid: String,
+    r#type: String,
+    uuid: String,
     name: String,
     issuer: String,
-    // note: String,
-    // favorite: bool,
+    note: String,
+    favorite: bool,
     // #[serde(skip)]
     // icon: String,
     info: EntryInfo,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize)]
 struct Database {
-    version: u32,
+    version: u32, // TODO: Check version
     entries: Vec<Entry>,
 }
 
-fn parse_aegis_json(path: &str) -> Value {
+#[derive(Debug, Deserialize)]
+struct KeyParams {
+    nonce: String,
+    tag: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Slot {
+    r#type: i32,
+    uuid: String,
+    key: String,
+    key_params: KeyParams,
+    n: Option<i32>,
+    r: Option<i32>,
+    p: Option<i32>,
+    salt: Option<String>,
+    repaired: Option<bool>,
+    is_backup: Option<bool>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Header {
+    slots: Vec<Slot>,
+    params: KeyParams,
+}
+
+#[derive(Debug, Deserialize)]
+struct AegisBackup {
+    version: u32, // TODO: Check version
+    header: Header,
+    db: String,
+}
+
+fn parse_aegis_json(path: &str) -> AegisBackup {
     let mut file = File::open(path).expect("Failed to open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
@@ -65,7 +96,11 @@ fn derive_key(password: &[u8], salt_hex: &str) -> Output {
     .expect("Failed to parse salt");
 
     // TODO: Parse parameters from JSON
-    let scrypt_params = scrypt::Params::new(15, 8, 1, 32).expect("Failed to set scrypt params");
+    let n = 15;
+    let r = 8;
+    let p = 1;
+
+    let scrypt_params = scrypt::Params::new(n, r, p, 32).expect("Failed to set scrypt params");
     let derived_key = Scrypt
         .hash_password_customized(password, None, None, scrypt_params, &salt)
         .expect("Failed to derive key");
@@ -94,7 +129,7 @@ fn main() -> Result<()> {
     color_eyre::install()?;
 
     let args: Vec<String> = env::args().collect();
-    let aegis_json = parse_aegis_json(args.get(1).expect("No filepath argument"));
+    let aegis = parse_aegis_json(args.get(1).expect("No filepath argument"));
 
     // TODO: Remove
     //let password = aegis_json["pw"].as_str().expect("No pw");
@@ -103,29 +138,20 @@ fn main() -> Result<()> {
         .interact()?;
     // TODO: Support password file
 
-    // TODO: Try different slots
-    let slot = 1;
+    // TODO: Try different slots.
+    //       Find the ones with type == 1
+    //       Iterate over them and decrypt the master key
+    let slot_index = 1;
+    let slot = aegis.header.slots.get(slot_index).unwrap();
 
     // Derive a key from the provided password and the salt from the file
-    let salt_hex_str = aegis_json["header"]["slots"][slot]["salt"]
-        .as_str()
-        .expect("Failed to parse salt string");
-    let derived_key = derive_key(password.as_bytes(), salt_hex_str);
-
-    let key_tag_hex = aegis_json["header"]["slots"][slot]["key_params"]["tag"]
-        .as_str()
-        .expect("Failed to find the tag");
-    let key_nonce = Vec::from_hex(
-        aegis_json["header"]["slots"][slot]["key_params"]["nonce"]
-            .as_str()
-            .expect("Failed to find the nonce"),
-    )
-    .expect("Failed to parse key nonce hex");
-    let master_key_enc_hex = aegis_json["header"]["slots"][slot]["key"]
-        .as_str()
-        .expect("Failed to find the key");
-    let mut master_key_enc: Vec<u8> = Vec::from_hex(master_key_enc_hex)?.to_vec();
-    master_key_enc.extend_from_slice(&Vec::from_hex(key_tag_hex)?);
+    let derived_key = derive_key(
+        password.as_bytes(),
+        slot.salt.as_ref().expect("Failed to parse salt string"),
+    );
+    let key_nonce = Vec::from_hex(&slot.key_params.nonce)?;
+    let mut master_key_enc: Vec<u8> = Vec::from_hex(&slot.key)?.to_vec();
+    master_key_enc.extend_from_slice(&Vec::from_hex(&slot.key_params.tag)?);
 
     // Decrypt master key
     let mut cipher = Aes256Gcm::new(derived_key.as_bytes().into());
@@ -134,28 +160,18 @@ fn main() -> Result<()> {
         .expect("Failed to decrypt master key");
 
     // Get database encryption parameters
-    let db_tag_hex = aegis_json["header"]["params"]["tag"]
-        .as_str()
-        .expect("Failed to find the db tag");
-    let db_nonce = Vec::from_hex(
-        aegis_json["header"]["params"]["nonce"]
-            .as_str()
-            .expect("Failed to find the db nonce"),
-    )
-    .expect("Failed to parse db nonce hex");
-    let db_contents_enc = general_purpose::STANDARD_NO_PAD.decode(
-        aegis_json["db"]
-            .as_str()
-            .expect("Failed to find the database"),
-    )?;
+    let db_tag = Vec::from_hex(aegis.header.params.tag)?;
+    let db_nonce = Vec::from_hex(aegis.header.params.nonce)?;
+    let db_contents_enc = general_purpose::STANDARD_NO_PAD.decode(aegis.db)?;
 
     // Decrypt database
     let mut cipher_db = Aes256Gcm::new(master_key.as_slice().into());
     let mut db_enc: Vec<u8> = db_contents_enc;
-    db_enc.extend_from_slice(&Vec::from_hex(db_tag_hex)?);
+    db_enc.extend_from_slice(&db_tag);
     let db_contents = cipher_db
         .decrypt(Nonce::from_slice(&db_nonce), db_enc.as_ref())
         .expect("Failed to decrypt database");
+
     let db_contents_str = String::from_utf8(db_contents).expect("UTF8");
     let db: Database = serde_json::from_str(&db_contents_str).expect("Failed to parse JSON");
 
